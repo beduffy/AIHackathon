37f3716a3c29e21ef1b06be9f53856c7db067af9
Duffy <benjamin.duffy@accenture.com>
2017-06-22 16:21:34 +0100

    newesst

--- main.py
+++ main.py
@@ -9,19 +9,20 @@ import pandas as pd
 
 pd.options.display.max_columns = 999
 
+from stop_words import stopwords
 
-companies = ['accenture', 'deloitte', 'ibm', 'capgemini', ' hp ', 'infosys',
-             'johnson & johnson', 'roche', 'pfizer', 'novartis', 'bayer',
-             'glaxosmithkline']
+companies = ['accenture', 'deloitte', 'ibm', 'capgemini',  'infosys',
+             'roche', 'pfizer', 'novartis', 'bayer', 'glaxosmithkline']
 
+             #' hp ', 'johnson & johnson',
 cols_to_extract = ['GLOBALEVENTID', 'SQLDATE', 'SOURCEURL', 'ActionGeo_Lat', 'ActionGeo_Long',
                    'ActionGeo_FullName', 'ActionGeo_CountryCode']
 extra_columns = ['company', 'industry', 'topic', 'sentiment']
 final_columns = cols_to_extract + extra_columns + ['title']
               
-with open('column_labels_2013+.txt') as f:
+'''with open('column_labels_2013+.txt') as f:
     col_labels = f.read().split('\t')
-    print col_labels
+    print col_labels'''
 
 def clean_title(path):
     path = path.replace('-', ' ').lower()
@@ -60,15 +61,15 @@ def get_company_rows(df, company):
     #print df_acn.head(20)
     
     df_comp['title'] = df_comp['SOURCEURL'].apply(get_title_comp_better, args=(company, ))
-    
+
     print 'Number of rows for company:', company, ':', df_comp.shape
-    if df_comp.shape[0] > 0:
-        print df_comp['title']
+    #if df_comp.shape[0] > 0:
+    #    print df_comp['title']
     return df_comp
 
 def get_topics(df):
     titles = df.tolist()
-    print titles
+    #print titles
     counter = collections.Counter()
     for title in titles:
         counter.update(title.split())
@@ -77,18 +78,25 @@ def get_topics(df):
         if word in counter:
             del counter[word]
             
-    print counter.most_common()
+    #print counter.most_common()
+    with open('topics_v2.txt', 'w') as f:
+        for k_v in counter_most_common():
+            print k_v
+            f.write(k_v + '\n')
+
     return counter
     
 def read_news_csv(fp):
-    #df = pd.read_csv(fp)
-    df = pd.read_table(fp, header=None)
-    df.columns = col_labels
+    df = pd.read_csv(fp)
+    #df = pd.read_table(fp, header=None)
+    #df.columns = cols_to_extract #col_labels
     #print df.head()
     #print df['SOURCEURL'].head()
-    print 'shape with duplicates:', df.shape
+    '''print 'shape with duplicates:', df.shape
     df = df.drop_duplicates(subset=['SOURCEURL'], keep='first')
-    print 'shape after dropping duplicates:', df.shape
+    print 'shape after dropping duplicates:', df.shape'''
+    #print list(df.columns.values)
+    print df.shape
     
     df['title'] = df['SOURCEURL'].map(get_title)
     #df['title'] = df['SOURCEURL'].apply(get_title, args=(,))
@@ -96,15 +104,16 @@ def read_news_csv(fp):
     #print df['title']
     #print df['title'].value_counts() # very important to see error cases
     
-    
-    
     df_companies = pd.DataFrame(columns=final_columns)
     for idx, company in enumerate(companies):
         df_comp = get_company_rows(df, company)
-        df_comp = df_comp[cols_to_extract]
+        #df_comp = df_comp[cols_to_extract]
         for col in extra_columns:
             df_comp[col] = ""
         df_comp['company'] = company
+        #df_comp['title'] = 
+        #if df_comp.shape[0] > 0:
+        #    print df_comp['title']
         
         df_companies = df_companies.append(df_comp)
     
@@ -112,20 +121,19 @@ def read_news_csv(fp):
     print 'df from single file shape', df_companies.shape
     del df
     return df_companies
-    
+
 def get_csv_paths(num_fetch):
-    path = 'data'   
+    path = 'data/merged_csvs'   
     onlyfiles = [f for f in os.listdir(path) if isfile(join(path, f))]
 
     #csv_file_paths = ['data/'+f for f in onlyfiles if f[-3:].lower() == 'csv']
-	csv_file_paths = ['data/merged_csvs/' + f for f in onlyfiles if f[-3:].lower() == 'csv']
+    csv_file_paths = ['data/merged_csvs/' + f for f in onlyfiles if f[-3:].lower() == 'csv']
 
     if num_fetch:
 		csv_file_paths = csv_file_paths[0:num_fetch]
     return csv_file_paths
 
 def create_merged_dataset(num_fetch):
-    dest_path = 'merged_big_dataset_{}.csv'
     csv_file_paths = get_csv_paths(num_fetch)
     
     df_all = pd.DataFrame(columns=final_columns)
@@ -140,10 +148,12 @@ def create_merged_dataset(num_fetch):
     
     get_topics(df_all['title'])
 
+    dest_path = 'merged_big_dataset_rows_{}.csv'.format(df_all.shape[0])
     df_all.to_csv(dest_path, index=True)
    
    
 if __name__ == "__main__":
-	create_merged_dataset(1)
+	create_merged_dataset(5)
+	#create_merged_dataset(3)
 	#create_merged_dataset(10)
     
